69f5965fb1cb276c0ad9c9ec195b3c5eb560c647
DavidV1984 <david.vegelj@gmail.com>
2019-12-25 14:35:17 +0100

    odstranitev funkcije za dnevni donos

--- mutual_fund_income.py
+++ mutual_fund_income.py
@@ -2,18 +2,13 @@
 # -*- coding: windows-1250 -*-
 
 import pandas as pd
-import random
 import numpy as np
 
 doba = 100
 
-def daily_return():
-    return np.random.normal(0.0038, 0.0098)
-
-
 df = pd.DataFrame(index=range(1, doba + 1))
 
-#df['Daily return'] = daily_return()
+# df['Daily return'] = daily_return()
 df['D_return'] = np.random.normal(0.0038, 0.0098, len(df))
 
 print(df)

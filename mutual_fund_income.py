1574e0e958838ffe0426494fbef3e9ef034daf6b
DavidV1984 <david.vegelj@gmail.com>
2019-12-25 14:47:34 +0100

    dodana random vplačila

--- mutual_fund_income.py
+++ mutual_fund_income.py
@@ -4,11 +4,19 @@
 import pandas as pd
 import numpy as np
 
-doba = 100
+# doba v dnevih (252 dni = 1 leto)
+doba = 252
 
+# upravljavska provizija na letni osnovi
+mngt_fee = 0.0012
+
+# ustvarimo dataframe z indeksom
 df = pd.DataFrame(index=range(1, doba + 1))
 
-# df['Daily return'] = daily_return()
+# ustvarimo vplaèila
+df['R_inflow'] = np.random.choice(a=[500, 1000, 2500, 5000, 10000, 15000, 25000, 50000, 100000], size=len(df))
+
+# dodamo random dnevne donose - podatki menda za S&P 500 (normalna distribucija)
 df['D_return'] = np.random.normal(0.0038, 0.0098, len(df))
 
 print(df)
